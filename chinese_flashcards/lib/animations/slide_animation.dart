dbe8560f78f27d762f003039ef0f4b45f7fb7663
gonzalezfranks <gonzalezfranks@hotmail.com>
2022-06-18 17:38:08 +0200

    feat: capitulo 10

---
+++ chinese_flashcards/lib/animations/slide_animation.dart
@@ -0,0 +1,94 @@
+import 'package:chinese_flashcards/enums/slide_direction.dart';
+import 'package:flutter/material.dart';
+
+class SlideAnimation extends StatefulWidget {
+  const SlideAnimation({
+    required this.child,
+    required this.direction,
+    this.animate = true,
+    Key? key}) : super(key: key);
+
+  final Widget child;
+  final SlideDirection direction;
+  final bool animate;
+
+
+  @override
+  State<SlideAnimation> createState() => _SlideAnimationState();
+}
+
+class _SlideAnimationState extends State<SlideAnimation> with SingleTickerProviderStateMixin{
+
+  late final AnimationController _animationController;
+  late final Animation<Offset> _animation;
+
+  @override
+  void initState() {
+
+    _animationController = AnimationController(
+        vsync: this,
+      duration: const Duration(milliseconds: 4000)
+    );
+
+    if(widget.animate){
+      _animationController.forward();
+    }
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant SlideAnimation oldWidget) {
+    if(widget.animate){
+      _animationController.forward();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    late final Animation<Offset> _animation;
+    Tween<Offset> tween;
+
+    switch(widget.direction){
+
+      case SlideDirection.leftAway:
+        tween = Tween<Offset>(begin: const Offset(0,0), end: const Offset(-1, 0));
+        break;
+
+      case SlideDirection.rightAway:
+        tween = Tween<Offset>(begin: const Offset(0,0), end: const Offset(1, 0));
+        break;
+
+      case SlideDirection.leftIn:
+        tween = Tween<Offset>(begin: const Offset(-1,0), end: const Offset(0, 0));
+        break;
+
+      case SlideDirection.rightIn:
+        tween = Tween<Offset>(begin: const Offset(1,0), end: const Offset(0, 0));
+        break;
+
+      case SlideDirection.upIn:
+        tween = Tween<Offset>(begin: const Offset(0,1), end: const Offset(0, 0));
+        break;
+
+      case SlideDirection.none:
+        tween = Tween<Offset>(begin: const Offset(0,0), end: const Offset(0, 0));
+        break;
+    }
+
+    _animation = tween.animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+
+    return SlideTransition(position: _animation,
+    child: widget.child,
+    );
+  }
+}
+
--- chinese_flashcards/lib/pages/flashcards_page.dart
+++ chinese_flashcards/lib/pages/flashcards_page.dart
@@ -1,4 +1,6 @@
+import 'package:chinese_flashcards/animations/slide_animation.dart';
 import 'package:chinese_flashcards/components/app/custom_app_bar.dart';
+import 'package:chinese_flashcards/enums/slide_direction.dart';
 import 'package:chinese_flashcards/providers/flashcards_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,13 +13,30 @@ class FlashCardsPage extends StatefulWidget {
 }
 
 class _FlashCardsPageState extends State<FlashCardsPage> {
+
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Consumer<FlashcardsNotifiers>(
-      builder: (_, notifier, __) => const Scaffold(
-        appBar: PreferredSize(
+      builder: (_, notifier, __) => Scaffold(
+        appBar: const PreferredSize(
             preferredSize: Size.fromHeight(56),
             child: CustomAppBar()
+        ),
+
+        body: SlideAnimation(
+            direction: SlideDirection.leftIn,
+            child: Center(
+              child: Container(
+                width: size.width * 0.9,
+                height: size.height * 0.70,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor
+                ),
+              ),
+            )
         ),
       ),
     );
